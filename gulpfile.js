02c68a1a0a51bf0a9f49dccf6a4909cb7f3ace46
Christian Fei <crifei93@gmail.com>
2014-11-01 14:42:22 +0100

    added karma growl reporter

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,23 @@ var gulp = require('gulp');
 var browserify = require('gulp-browserify');
 var rename = require('gulp-rename');
 
-var browserifyEntryPoint = 'src/jekyll-search.js';
+var browserifyEntryPoint = 'src/index.js';
+
+
+
+
+
+gulp.task('default', ['js:src','js:test:unit'])
+
+gulp.task('watch', function(){
+  gulp.watch(['!'+browserifyEntryPoint,'src/**/*.js'], ['js:src','js:test:unit']);
+  gulp.watch(['!test/unit/browserifiedTests.js','test/unit/**/*.js'], ['js:test:unit']);
+});
+
+
+
+
+
 
 gulp.task('js:src', function() {
   gulp.src(browserifyEntryPoint)
@@ -21,11 +37,4 @@ gulp.task('js:test:unit', function() {
     }))
     .pipe(rename('browserifiedTests.js'))
     .pipe(gulp.dest('./test/unit/'))
-});
-
-gulp.task('default', ['js:src','js:test:unit'])
-
-gulp.task('watch', function(){
-  gulp.watch(['!'+browserifyEntryPoint,'src/**/*.js'], ['js:src','js:test:unit']);
-  gulp.watch(['!test/unit/browserifiedTests.js','test/unit/**/*.js'], ['js:test:unit']);
 });
