80ab24eaf8fe90a81186f6532812bddb462ab102
Christian Fei <crifei93@gmail.com>
2014-11-01 15:28:52 +0100

    added concat gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,8 @@
 var gulp = require('gulp');
 var browserify = require('gulp-browserify');
 var rename = require('gulp-rename');
+var using = require('gulp-using');
+var concat = require('gulp-concat');
 
 var browserifyEntryPoint = 'src/index.js';
 
@@ -10,9 +12,9 @@ var browserifyEntryPoint = 'src/index.js';
 
 gulp.task('default', ['js:src','js:test:unit'])
 
-gulp.task('watch', function(){
+gulp.task('watch',['default'], function(){
   gulp.watch(['!'+browserifyEntryPoint,'src/**/*.js'], ['js:src','js:test:unit']);
-  gulp.watch(['!test/unit/browserifiedTests.js','test/unit/**/*.js'], ['js:test:unit']);
+  gulp.watch(['!test/unit/browserifiedTests.js','test/unit/**/*Test.js'], ['js:test:unit']);
 });
 
 
@@ -30,11 +32,13 @@ gulp.task('js:src', function() {
 });
 
 gulp.task('js:test:unit', function() {
-  gulp.src('test/unit/**/*Test.js')
+  gulp.src(['test/unit/**/*Test.js'])
+    .pipe(using())
+    .pipe(concat('browserifiedTests.js'))
     .pipe(browserify({
       // insertGlobals : true,
       debug : !process.env.PROD
     }))
-    .pipe(rename('browserifiedTests.js'))
+    .pipe(using())
     .pipe(gulp.dest('./test/unit/'))
 });
