e8a5fdedfcd48ec06ea52b8eb5956502fcaa70a6
Christian Fei <crifei93@gmail.com>
2014-11-02 22:44:53 +0100

    ref single speaking methods JSONLoader

--- src/JSONLoader.js
+++ src/JSONLoader.js
@@ -2,18 +2,26 @@ module.exports = new JSONLoader();
 function JSONLoader(){
   var self = this;
 
-  self.load = function(location,callback){
-    var xhr = (window.XMLHttpRequest) ? new XMLHttpRequest() : new ActiveXObject("Microsoft.XMLHTTP");
-    xhr.open("GET", location, true);
+  function receivedResponse(xhr){
+    return xhr.status==200 && xhr.readyState==4;
+  }
+
+  function handleResponse(xhr,callback){
     xhr.onreadystatechange = function(){
-      if (xhr.status==200 && xhr.readyState==4){
+      if ( receivedResponse(xhr) ){
         try{
           callback(null,JSON.parse(xhr.responseText) );
         }catch(err){
           callback(err,null);
-        }
+        }    
       }
     }
+  }
+
+  self.load = function(location,callback){
+    var xhr = (window.XMLHttpRequest) ? new XMLHttpRequest() : new ActiveXObject("Microsoft.XMLHTTP");
+    xhr.open("GET", location, true);
+    handleResponse(xhr,callback);
     xhr.send();
   };
 };
