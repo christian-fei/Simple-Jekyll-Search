57d9e28b574a2f6279d6ebd9312f710d8de2c773
Christian Fei <crifei93@gmail.com>
2014-11-02 22:33:04 +0100

    cleanup

--- src/SearchStrategies/fuzzy.js
+++ src/SearchStrategies/fuzzy.js
@@ -3,7 +3,7 @@ module.exports = new FuzzySearchStrategy();
 function FuzzySearchStrategy(){
   var self = this;
 
-  this.matches = function(string,crit){
+  self.matches = function(string,crit){
     var regexp = new RegExp( crit.split('').join('.*?'), 'gi');
     return !!string.match(regexp);
   };
--- src/SearchStrategies/literal.js
+++ src/SearchStrategies/literal.js
@@ -2,7 +2,8 @@ module.exports = new LiteralSearchStrategy();
 
 function LiteralSearchStrategy(){
   var self = this;
-  this.matches = function(string,crit){
+
+  self.matches = function(string,crit){
     return string.toLowerCase().indexOf(crit.toLowerCase()) >= 0;
   };
 };
