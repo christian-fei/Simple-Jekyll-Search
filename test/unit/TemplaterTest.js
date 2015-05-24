a6d362f01f238474c20b99b5f92604168425550d
Christian Fei <crifei93@gmail.com>
2015-05-24 17:11:09 +0200

    removed ;

--- test/unit/TemplaterTest.js
+++ test/unit/TemplaterTest.js
@@ -1,33 +1,33 @@
 describe("Templater", function() {
-  var Templater = require('../../src/Templater.js');
-  var templater;
+  var Templater = require('../../src/Templater.js')
+  var templater
 
   beforeEach(function() {
-    templater = new Templater;
-  });
+    templater = new Templater
+  })
 
   it("should render the template with the provided data", function() {
     expect(
       templater.render('{foo}',{foo:'bar'})
     ).toEqual(
       'bar'
-    );
+    )
 
     expect(
       templater.render('<a href="{url}">url</a>',{url:'http://google.com'})
     ).toEqual(
       '<a href="http://google.com">url</a>'
-    );
-  });
+    )
+  })
 
   it("should replace not found properties with the original pattern", function() {
-    var template = '{foo}';
+    var template = '{foo}'
     expect(
       templater.render(template,{x:'bar'})
     ).toEqual(
       template
-    );
-  });
+    )
+  })
 
   it("should allow custom patterns to be set", function() {
     templater = new Templater({
@@ -37,6 +37,6 @@ describe("Templater", function() {
       templater.render('{{foo}}',{foo:'bar'})
     ).toEqual(
       'bar'
-    );
-  });
-});
+    )
+  })
+})
