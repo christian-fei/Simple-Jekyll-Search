d670819aa9971f224b78381a6e22f21559dbd65f
Christian Fei <crifei93@gmail.com>
2015-08-07 20:37:42 +0200

    cleaned up gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,14 @@
-var gulp = require('gulp');
-var browserify = require('gulp-browserify');
-var rename = require('gulp-rename');
-var using = require('gulp-using');
-var concat = require('gulp-concat');
-var uglify = require('gulp-uglify');
-var karma = require('gulp-karma');
+var gulp = require('gulp')
+  , browserify = require('gulp-browserify')
+  , rename = require('gulp-rename')
+  , using = require('gulp-using')
+  , concat = require('gulp-concat')
+  , uglify = require('gulp-uglify')
+  , karma = require('gulp-karma')
 
-var browserifyEntryPoint = 'src/index.js';
-
+var ENTRYPOINT = {
+  JS: 'src/index.js'
+}
 
 
 
@@ -15,9 +16,9 @@ var browserifyEntryPoint = 'src/index.js';
 gulp.task('default', ['js:src','js:test:unit'])
 
 gulp.task('watch',['default'], function(){
-  gulp.watch(['!'+browserifyEntryPoint,'src/**/*.js'], ['js:src','js:test:unit']);
-  gulp.watch(['test/unit/**/*.js'], ['js:test:unit']);
-});
+  gulp.watch(['!'+ENTRYPOINT.JS,'src/**/*.js'], ['js:src','js:test:unit'])
+  gulp.watch(['test/unit/**/*.js'], ['js:test:unit'])
+})
 
 
 
@@ -25,14 +26,14 @@ gulp.task('watch',['default'], function(){
 
 
 gulp.task('js:src', function() {
-  gulp.src(browserifyEntryPoint)
+  gulp.src(ENTRYPOINT.JS)
     .pipe(browserify({
       debug : !process.env.PROD
     }))
     .pipe(uglify({mangle: false,compress:true}))
     .pipe(rename('jekyll-search.js'))
     .pipe(gulp.dest('./dest/'))
-});
+})
 
 gulp.task('js:test:unit', function() {
   return gulp.src(['test/unit/**/*.js'])
@@ -44,4 +45,4 @@ gulp.task('js:test:unit', function() {
       this.emit('end')
       throw err
     })
-});
+})
