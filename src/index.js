3bd258056822469bd94b65cc0e81bbc87382a157
Eli Gladman <eli.gladman@gmail.com>
2016-01-09 11:44:56 -0500

    added 'enter' key to whitelist

--- src/index.js
+++ src/index.js
@@ -94,7 +94,7 @@
     options.searchInput.addEventListener('keyup', debounce(function(e){
 
       // whitelist the following keycodes
-      var whitelist = [37,38,39,40];
+      var whitelist = [13,37,38,39,40];
 
       var keyCodes = whitelist.map(function(code){
         return 'e.which != ' + code;
