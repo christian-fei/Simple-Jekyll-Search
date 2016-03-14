f62501847c15b02c1b1df2da9271c54cf45db858
June <junedev@users.noreply.github.com>
2016-03-14 20:49:19 +0100

    Test cases added as requested

--- src/SearchStrategies/FuzzySearchStrategy.test.js
+++ src/SearchStrategies/FuzzySearchStrategy.test.js
@@ -16,7 +16,12 @@ describe('FuzzySearchStrategy', function () {
   it('matches fuzzy', function () {
     expect( FuzzySearchStrategy.matches('fuzzy','fzy') ).toEqual(true)
     expect( FuzzySearchStrategy.matches('react','rct') ).toEqual(true)
-
     expect( FuzzySearchStrategy.matches('what the heck','wth?') ).toEqual(true)
+  })
+  
+  it('does not match all strings just because some letters match', function () {
+    expect( FuzzySearchStrategy.matches('active', 'aaaaaa') ).toEqual(false)
+    expect( FuzzySearchStrategy.matches('firebase', 'fe') ).toEqual(false),
+    expect( FuzzySearchStrategy.matches('this should not be matched', 'tsnd') ).toEqual(false)
   })
 })
