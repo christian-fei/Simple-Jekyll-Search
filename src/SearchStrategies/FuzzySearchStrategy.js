35e666c276f00261cbbb229896c030f0f5d8815e
Christian Fei <crifei93@gmail.com>
2016-08-25 21:52:09 +0200

    use fuzzysearch module

--- src/SearchStrategies/FuzzySearchStrategy.js
+++ src/SearchStrategies/FuzzySearchStrategy.js
@@ -1,14 +1,10 @@
 'use strict'
+var fuzzysearch = require('fuzzysearch')
+
 module.exports = new FuzzySearchStrategy()
 
 function FuzzySearchStrategy(){
   this.matches = function(string, crit){
-    if( typeof string !== 'string' || typeof crit !== 'string' ){
-      return false
-    }
-    var fuzzy = crit.split("")
-                .reduce(function(a,b){ return a+'[^'+b+']*'+b; })
-    fuzzy = new RegExp( fuzzy, 'gi')
-    return !!fuzzy.test(string)
+    return fuzzysearch(crit, string)
   }
 }
--- src/SearchStrategies/FuzzySearchStrategy.test.js
+++ src/SearchStrategies/FuzzySearchStrategy.test.js
@@ -8,17 +8,17 @@ describe('FuzzySearchStrategy', function () {
     FuzzySearchStrategy = require('./FuzzySearchStrategy')
   })
 
-  it('matches only on strings', function () {
-    expect( FuzzySearchStrategy.matches({},'') ).to.eql(false)
-    expect( FuzzySearchStrategy.matches(false,'') ).to.eql(false)
-    expect( FuzzySearchStrategy.matches(true,'') ).to.eql(false)
-    expect( FuzzySearchStrategy.matches(1,'') ).to.eql(false)
+  it('does not match words that don\'t contain the search criteria', function () {
+    expect( FuzzySearchStrategy.matches('fuzzy','fzyyy') ).to.eql(false)
+    expect( FuzzySearchStrategy.matches('react','angular') ).to.eql(false)
+
+    expect( FuzzySearchStrategy.matches('what the heck','wth?') ).to.eql(false)
   })
 
-  it('matches fuzzy', function () {
+  it('matches words containing the search criteria', function () {
     expect( FuzzySearchStrategy.matches('fuzzy','fzy') ).to.eql(true)
     expect( FuzzySearchStrategy.matches('react','rct') ).to.eql(true)
 
-    expect( FuzzySearchStrategy.matches('what the heck','wth?') ).to.eql(true)
+    expect( FuzzySearchStrategy.matches('what the heck','wth') ).to.eql(true)
   })
 })
