0c6a87ffd9b7bf9838bc7d875e8d33c61c1a2555
Christian <crifei93@gmail.com>
2018-11-10 19:07:28 +0100

    grammar

--- tests/SearchStrategies/LiteralSearchStrategy.test.js
+++ tests/SearchStrategies/LiteralSearchStrategy.test.js
@@ -5,15 +5,15 @@ const {equal} = require('assert')
 const LiteralSearchStrategy = require('../../src/SearchStrategies/LiteralSearchStrategy')
 
 test('LiteralSearchStrategy', () => {
-  test('matches a word that are contained in the search criteria (single word)', () => {
+  test('matches a word that is contained in the search criteria (single words)', () => {
     equal(LiteralSearchStrategy.matches('hello world test search text', 'world'), true)
   })
 
-  test('don\'t matche a search that contain a word that is not in the text', () => {
+  test('does not match if a word is not contained in the search criteria', () => {
     equal(LiteralSearchStrategy.matches('hello world test search text', 'hello my world'), false)
   })
 
-  test('matches a word that are contained in the search criteria (multiple word)', () => {
+  test('matches a word that is contained in the search criteria (multiple words)', () => {
     equal(LiteralSearchStrategy.matches('hello world test search text', 'hello text world'), true)
   })
 })
