01481b1ef296cc0217fcb5d5e55ebb73ee7afe8a
sylhare <sylhare@outlook.com>
2021-04-05 21:42:02 -0400

    Add exact match with space usecase

--- tests/SearchStrategies/LiteralSearchStrategy.test.js
+++ tests/SearchStrategies/LiteralSearchStrategy.test.js
@@ -13,3 +13,19 @@ test('does not match if a word is not contained in the search criteria', t => {
 test('matches a word that is contained in the search criteria (multiple words)', t => {
   t.deepEqual(LiteralSearchStrategy.matches('hello world test search text', 'hello text world'), true)
 })
+
+test('matches exact words when exacts words with space in the search criteria', t => {
+  t.deepEqual(LiteralSearchStrategy.matches('hello world test search text', 'hello world '), true)
+})
+
+test('does not matches multiple words if not exact words with space in the search criteria', t => {
+  t.deepEqual(LiteralSearchStrategy.matches('hello world test search text', 'hello text world '), false)
+})
+
+test('matches a word that is partially contained in the search criteria', t => {
+  t.deepEqual(LiteralSearchStrategy.matches('this tasty tester text', 'test'), true)
+})
+
+test('does not matches a word that is partially contained in the search criteria when followed by a space', t => {
+  t.deepEqual(LiteralSearchStrategy.matches('this tasty tester text', 'test '), false)
+})
