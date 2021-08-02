a94dcc3c9073f2386180fd5520ba34afac00b834
Jonas Kohl <github@jonaskohl.de>
2021-08-02 20:01:11 +0200

    Fixed null exception with fuzzy search

--- src/SearchStrategies/FuzzySearchStrategy.js
+++ src/SearchStrategies/FuzzySearchStrategy.js
@@ -6,6 +6,9 @@ module.exports = new FuzzySearchStrategy()
 
 function FuzzySearchStrategy () {
   this.matches = function (string, crit) {
+    if (string === null) {
+      return false
+    }
     return fuzzysearch(crit.toLowerCase(), string.toLowerCase())
   }
 }
